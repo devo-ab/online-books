69f6fc07d260fb1365cdc8a314bde95e5b0fb40b
designer-ab <per.useravi0@gmail.com>
2024-03-26 00:33:46 +0600

    add footer and contact with reponsive

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,8 +1,29 @@
 const Contact = () => {
     return (
-        <div>
-            <h1>contact</h1>
-        </div>
+        <div className="grid max-w-screen-xl grid-cols-1 gap-8 px-8 py-16 mx-auto rounded-lg md:grid-cols-2 md:px-12 lg:px-16 xl:px-32 bg-white text-black mt-10">
+	<div className="flex flex-col justify-between">
+		<div className="space-y-2">
+			<h2 className="text-4xl font-bold leading-tight lg:text-5xl">Let's talk!</h2>
+			<div className="dark:text-gray-600">Send your message or feedback</div>
+		</div>
+		<img src="../../../public/contact.png" alt="" className="p-4 h-52 md:h-64" />
+	</div>
+	<form className="space-y-6">
+		<div>
+			<label className="text-sm">Full name</label>
+			<input id="name" type="text" placeholder="" className="w-full p-3 rounded bg-gray-300" />
+		</div>
+		<div>
+			<label className="text-sm">Email</label>
+			<input id="email" type="email" className="w-full p-3 rounded dark:bg-gray-300" />
+		</div>
+		<div>
+			<label className="text-sm">Message</label>
+			<textarea id="message" rows="3" className="w-full p-3 rounded dark:bg-gray-300" ></textarea>
+		</div>
+		<button type="submit" className="w-full btn p-3 text-sm font-bold tracking-wide uppercase rounded dark:bg-violet-600 dark:text-gray-50">Send Message</button>
+	</form>
+</div>
     );
 };
 
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,36 @@
 const Footer = () => {
-    return (
-        <div>
-            <h1>Footer</h1>
-        </div>
-    );
+  return (
+    <footer className="footer p-10 bg-base-200 text-base-content mt-10">
+      <aside>
+        <img className="w-24" src="../../../public/online-books-logo.png" alt="" />
+        <p>
+          Online Books Ltd.
+          <br />
+          Providing reliable tech since 2024
+        </p>
+      </aside>
+      <nav>
+        <h6 className="footer-title">Services</h6>
+        <a className="link link-hover">Books Rent</a>
+        <a className="link link-hover">PDF Books</a>
+        <a className="link link-hover">Student Help</a>
+        <a className="link link-hover">Advertisement</a>
+      </nav>
+      <nav>
+        <h6 className="footer-title">Company</h6>
+        <a className="link link-hover">About us</a>
+        <a className="link link-hover">Contact</a>
+        <a className="link link-hover">Upcoming Books</a>
+        <a className="link link-hover">Press kit</a>
+      </nav>
+      <nav>
+        <h6 className="footer-title">Legal</h6>
+        <a className="link link-hover">Terms of use</a>
+        <a className="link link-hover">Privacy policy</a>
+        <a className="link link-hover">Cookie policy</a>
+      </nav>
+    </footer>
+  );
 };
 
 export default Footer;
