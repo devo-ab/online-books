9d650f2a91f380b64228d2019748269a152d1451
designer-ab <per.useravi0@gmail.com>
2024-03-26 15:02:43 +0600

    add about page with responsive

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,9 +1,19 @@
 const About = () => {
-    return (
-        <div>
-            <h1>about</h1>
-        </div>
-    );
+  return (
+    <div className="lg:w-[1170px] mx-auto lg:mt-10 px-2 lg:px-0">
+      <h1 className="text-2xl lg:text-4xl text-black font-semibold">About Online Books</h1>
+      <p className="mt-2 lg:mt-5">
+        Online Books is a company founded in 2021, and was acquired by Netlify in 2022.Onlines Books
+        is a critical part of the student community and helps support one of the largest amount of
+        student to read books in the world.
+      </p>
+      <p className="mt-3 lg:mt-5 font-medium">Online Book is lots of things.</p>
+      <li>Online Books is a library.It is the store of huge amount of books.</li>
+      <li>It is the public collection of books,here student can read books free.</li>
+      <li>Responsive design for mobile devices.</li>
+      <li>Analytics for collection development and user engagement.</li>
+    </div>
+  );
 };
 
 export default About;
